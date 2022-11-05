8dc33d6fc05e53760806d68627788994971d4c44
sadikhossain69 <sadikhossain433@gmail.com>
2022-11-06 01:36:15 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const PORT = 5000 || process.env.PORT
+
+// Middlewares 
+app.use(cors())
+app.use(express.json())
