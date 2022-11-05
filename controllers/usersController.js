c14d072003cae993abba43f2acfa3a9c27e376a6
sadikhossain69 <sadikhossain433@gmail.com>
2022-11-06 02:02:54 +0600

    Random User Api Created

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,13 @@
+const Users = require("../utils/Users")
+
+/**
+ * It takes a request and a response, and sends a random user from the Users array.
+ * @param req - request
+ * @param res - The response object.
+ */
 const randomUser = async (req, res) => {
-    console.log("Random User")
-    res.send("Random User")
+    const random = Users[Math.floor(Math.random() * Users.length)]
+    res.send(random)
 }
 
 module.exports = {
---
+++ utils/Users.js
@@ -0,0 +1,64 @@
+let Users = [
+    {
+        "id": 1,
+        "photoUrl": "http://placehold.it/32x32",
+        "age": 31,
+        "name": "Rachelle Alford",
+        "gender": "female",
+        "email": "rachellealford@concility.com",
+        "contact": "+1 (993) 495-2453",
+        "address": "277 Beard Street, Fairview, Indiana, 8616"
+    },
+    {
+        "id": 2,
+        "photoUrl": "http://placehold.it/32x32",
+        "age": 29,
+        "name": "Alba Sargent",
+        "gender": "female",
+        "email": "albasargent@concility.com",
+        "contact": "+1 (800) 541-3229",
+        "address": "317 Lewis Avenue, Harrison, Massachusetts, 9841"
+    },
+    {
+        "id": 3,
+        "photoUrl": "http://placehold.it/32x32",
+        "age": 25,
+        "name": "Pope Bartlett",
+        "gender": "male",
+        "email": "popebartlett@concility.com",
+        "contact": "+1 (862) 443-3309",
+        "address": "465 Sutton Street, Nutrioso, Virgin Islands, 7224"
+    },
+    {
+        "id": 4,
+        "photoUrl": "http://placehold.it/32x32",
+        "age": 40,
+        "name": "Charity Donovan",
+        "gender": "female",
+        "email": "charitydonovan@concility.com",
+        "contact": "+1 (800) 571-3573",
+        "address": "333 Wolf Place, Laurelton, Pennsylvania, 6183"
+    },
+    {
+        "id": 5,
+        "photoUrl": "http://placehold.it/32x32",
+        "age": 30,
+        "name": "Andrews Mclaughlin",
+        "gender": "male",
+        "email": "andrewsmclaughlin@concility.com",
+        "contact": "+1 (892) 537-2638",
+        "address": "860 Independence Avenue, Dowling, Washington, 3505"
+    },
+    {
+        "id": 6,
+        "photoUrl": "http://placehold.it/32x32",
+        "age": 39,
+        "name": "Brooks Beck",
+        "gender": "male",
+        "email": "brooksbeck@concility.com",
+        "contact": "+1 (872) 572-2997",
+        "address": "567 Central Avenue, Clarksburg, South Carolina, 7995"
+    }
+]
+
+module.exports = Users
