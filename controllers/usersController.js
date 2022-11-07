e9dd50cb99d9a442547e431999efdc99fa1542f9
sadikhossain69 <sadikhossain433@gmail.com>
2022-11-07 22:21:10 +0600

    updateUser data created

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -10,6 +10,7 @@ const randomUser = async (req, res) => {
     res.send(random)
 }
 
+
 /**
  * It takes a request and a response, and sends a response with a limited number of users.
  * @param req - request
@@ -24,6 +25,7 @@ const allUsers = async (req, res) => {
     }
     res.send(Users)
 }
+
 
 /**
  * It takes the request body, and if it doesn't have all the required fields, it sends a message to the
@@ -42,8 +44,45 @@ const saveUser = async (req, res) => {
     res.send("User post Successfully")
 }
 
+
+/**
+ * It takes the id of the user from the request params, the data to be updated from the request body,
+ * finds the user with the id from the request params, and updates the user with the data from the
+ * request body.
+ * @param req - request
+ * @param res - the response object
+ */
+const updateUser = async (req, res) => {
+    const { id } = req.params;
+    const insertedData = req.body
+    const updatedData = Users.find(user => user.id === Number(id))
+
+    if (insertedData.name) {
+        updatedData.name = insertedData.name
+    }
+
+    if (insertedData.gender) {
+        updatedData.gender = insertedData.gender
+    }
+
+    if (insertedData.contact) {
+        updatedData.contact = insertedData.contact
+    }
+
+    if (insertedData.address) {
+        updatedData.address = insertedData.address
+    }
+
+    if (insertedData.photoUrl) {
+        updatedData.photoUrl = insertedData.photoUrl
+    }
+
+    res.send(updatedData)
+}
+
 module.exports = {
     randomUser,
     allUsers,
     saveUser,
+    updateUser,
 } 
--- routes/v1/userRoutes.js
+++ routes/v1/userRoutes.js
@@ -1,9 +1,15 @@
 const express = require('express')
-const { randomUser, allUsers, saveUser } = require('../../controllers/usersController')
+const {
+    randomUser,
+    allUsers,
+    saveUser,
+    updateUser
+} = require('../../controllers/usersController')
 const router = express.Router()
 
 router.route('/random').get(randomUser)
 router.route('/all').get(allUsers)
 router.route('/save').post(saveUser)
+router.route('/update/:id').patch(updateUser)
 
 module.exports = router
